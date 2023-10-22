4e9ca14c2ac7eaa1f927c806c1ef9851c17dbd3f
Liza <yelizaveta.ponomareva.2003@mail.ru>
2023-10-22 12:37:19 +0300

    Initial commit

---
+++ src/main/java/by/bsuir/caloriestracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package by.bsuir.caloriestracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CaloriesTrackerApplication.class);
+    }
+
+}
---
+++ src/test/java/by/bsuir/caloriestracker/CaloriesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.caloriestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
